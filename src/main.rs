ebfa9492518f16dae0448178748dbb46a10586c4
Dennis Collective <dennis.collective@gmail.com>
2017-08-23 23:36:20 -0600

    Deserialize image from json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Image {
+    index: i32,
+    url: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let json = "{
+           \"index\": 0,
+           \"url\": \"cowboyparty.com\"
+         }";
+    let image: Image = serde_json::from_str(&json).expect("failboat");
+    println!("deserialized = {:?}", image);
 }
