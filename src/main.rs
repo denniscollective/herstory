c863b4d4ec5a56d046618e66bc7cd1bd53bf7308
Dennis Collective <dennis.collective@gmail.com>
2017-08-25 16:56:35 -0600

    Use curl for http to get a handle on lower level syntax

--- src/main.rs
+++ src/main.rs
@@ -3,59 +3,38 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
-extern crate futures;
-extern crate hyper;
-extern crate tokio_core;
+extern crate curl;
 
-use std::io::{self, Write};
-use futures::{Future, Stream};
-use hyper::Client;
-use tokio_core::reactor::Core;
+use std::io::{stdout, Write};
+use curl::easy::Easy;
 
 fn main() {
     let photoset = deserialize_set(&get_json());
-    store_photoset(photoset);
+    photoset.store();
     println!("Done");
 }
 
-fn store_photoset (photoset: Photoset) -> () {
-    let mut core = Core::new().unwrap();
-    let client = Client::new(&core.handle());
-    for image in &photoset.images {
-        let uri = image.url.parse().unwrap();
-
-        let work = client.get(uri).map(|res| {
-            println!("Response: {}", res.status());
-            // res.body().for_each(|chunk| {
-                // io::stdout()
-                    // .write_all(&chunk)
-                    // .map_err(From::from)
-            // })
-        });
-        core.run(work).unwrap()
+impl Photoset {
+    fn store(&self) -> Vec<Easy> {
+        let mut requests: Vec<Easy> = Vec::new();
+        for image in &self.images {
+            let mut request = build_request(&image.url);
+            request.perform().unwrap();
+            println!("{}", request.response_code().unwrap());
+            requests.push(request);
+        }
+        requests
     }
 }
 
-// fn store_photoset(photoset: Photoset, client: &Client<hyper::client::HttpConnector>, core: &tokio_core::reactor::Core ){
-//     let image_futures: &mut Vec<hyper::client::FutureResponse> = &mut Vec::new();
-//     for image in &photoset.images {
-//         let work = client.get(image.url.parse().unwrap());
-//
-//         println!("{:?} - {:?}", image.index, &work);
-//         image_futures.push(work)
-//     }
-//
-//     for image in image_futures {
-//         image.wait();
-//
-//         image.and_then(|res| {
-//             println!("Response: {}", res.status());
-//         });
-//
-//
-//     }
-//     println!("deserialized = {:?}", photoset);
-// }
+fn build_request(uri: &str) -> Easy {
+    let mut request = Easy::new();
+    request.url(uri).unwrap();
+    // request.write_function(|data| {
+    //     Ok(stdout().write(data).unwrap())
+    // }).unwrap();
+    request
+}
 
 fn deserialize_set(json: &str) -> Photoset {
     serde_json::from_str(json).unwrap()
