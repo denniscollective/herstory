074cda5a068c730597d1578f7209e3f93df5b20e
Dennis Collective <dennis.collective@gmail.com>
2017-08-25 17:54:35 -0600

    Move bulk data processing functions to be methods
    Starting to grok the borrow checker. Maybe. Making structs from the deserialized ones,
    added this level of indirection because I couldn't figure out how to make Serde not
    freakout with the http requests on the structs. It would be nice to figue out how to
    make structs that wrap the other structs more gracefully, something like derrived data,
    or *gasp* struct inheritance? genericslol?
    
    It took a while to get the mutability and borrow annotations correct.
    (Part of) The solution wound up being to make the top level instanciation let mut,
    apparently in debugging these issues start futher up the chain?

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,74 @@ use std::io::{stdout, Write};
 use curl::easy::Easy;
 
 fn main() {
-    let photoset = deserialize_set(&get_json());
-    photoset.store();
-    println!("Done");
-}
-
-impl Photoset {
-    fn store(&self) -> Vec<Easy> {
-        let mut requests: Vec<Easy> = Vec::new();
-        for image in &self.images {
-            let mut request = build_request(&image.url);
-            request.perform().unwrap();
-            println!("{}", request.response_code().unwrap());
-            requests.push(request);
-        }
-        requests
-    }
+    let mut photoset = deserialize_set(&get_json());
+    photoset.perform_requests();
+    println!("{:?}", &photoset)
 }
 
 fn build_request(uri: &str) -> Easy {
     let mut request = Easy::new();
     request.url(uri).unwrap();
-    // request.write_function(|data| {
-    //     Ok(stdout().write(data).unwrap())
-    // }).unwrap();
     request
 }
 
+#[derive(Debug)]
+struct Photoset {
+ name: String,
+ images: Vec<Image>,
+}
+
+#[derive(Debug)]
+struct Image {
+    index: i32,
+    url: String,
+    request: Easy,
+}
+
+impl Image {
+    fn from(image: DeserializedImage) -> Image {
+        let request = build_request(&image.url);
+
+        Image {
+            request,
+            url: image.url,
+            index: image.index
+        }
+    }
+
+    fn perform_request(&mut self) {
+        self.request.write_function(|data| {
+            Ok(stdout().write(data).unwrap())
+        }).unwrap();
+        self.request.perform().unwrap();
+        println!("{}", self.request.response_code().unwrap());
+    }
+
+}
+
+impl Photoset {
+    fn from(photoset: DeserializedPhotoset) -> Photoset {
+        let mut images: Vec<Image> = Vec::new();
+        for image in photoset.images {
+            images.push(Image::from(image))
+        }
+
+        Photoset {
+            images,
+            name: photoset.name
+        }
+    }
+
+    fn perform_requests(&mut self) {
+        for image in &mut self.images {
+            image.perform_request()
+        }
+    }
+}
+
 fn deserialize_set(json: &str) -> Photoset {
-    serde_json::from_str(json).unwrap()
+    let photoset: DeserializedPhotoset = serde_json::from_str(json).unwrap();
+    Photoset::from(photoset)
 }
 
 fn get_json() -> String {
@@ -59,13 +98,13 @@ fn get_json() -> String {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Photoset {
+struct DeserializedPhotoset {
     name: String,
-    images: Vec<Image>,
+    images: Vec<DeserializedImage>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Image {
+struct DeserializedImage {
     index: i32,
-    url: String,
+    url: String
 }
