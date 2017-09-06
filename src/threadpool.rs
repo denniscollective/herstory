98e65d091a0588ac187fa2164d8e70fd7468c3f1
Dennis Collective <dennis.collective@gmail.com>
2017-09-06 04:18:24 -0600

    tasks execute themseleves, maybe this isn't correct but easy to change

--- src/threadpool.rs
+++ src/threadpool.rs
@@ -1,46 +1,26 @@
 use std::thread;
 use std::sync::{Arc, Mutex};
 
-pub struct Threadpool {
-    handles: Vec<thread::JoinHandle<()>>,
-}
+pub struct Threadpool {}
 
 impl Threadpool {
     pub fn new(_worker_size: u32) -> Threadpool {
-        Threadpool { handles: Vec::new() }
+        Threadpool {}
     }
 
-    fn execute<F, G>(&mut self, task: Task<G, F>)
-    where
-        F: Fn(&mut G) + Send + Sync + 'static,
-        G: Send + 'static,
-    {
-        self.handles.push(thread::spawn(move || task.execute()));
-    }
-
-    pub fn batch<F, G>(mut self, collection: &Vec<Arc<Mutex<G>>>, func: F)
+    pub fn batch<F, G>(self, collection: &Vec<Arc<Mutex<G>>>, func: F)
     where
         F: Fn(&mut G) + Send + Sync + 'static,
         G: Send + 'static,
     {
         let func = Arc::new(func);
-        collection
+        let tasks: Vec<thread::JoinHandle<()>> = collection
             .iter()
-            .map(|i| {
+            .map(|i| Task::clone_and_execute(&i, &func))
+            .collect();
 
-                self.execute(Task {
-                    item: i.clone(),
-                    func: func.clone(),
-                })
-            })
-            .count();
-
-        self.wait();
-    }
-
-    fn wait(self) {
-        for handle in self.handles {
-            handle.join().unwrap();
+        for handle in tasks {
+            handle.join().unwrap()
         }
     }
 }
@@ -52,10 +32,20 @@ struct Task<G, H> {
 
 impl<G, H> Task<G, H>
 where
-    H: Fn(&mut G),
+    H: Fn(&mut G) + Send + Sync + 'static,
+    G: Send + 'static,
 {
     fn execute(&self) {
         let mut i = self.item.lock().unwrap();
         (self.func)(&mut i)
+    }
+
+    fn clone_and_execute(i: &Arc<Mutex<G>>, func: &Arc<H>) -> thread::JoinHandle<()> {
+        let task = Task {
+            item: i.clone(),
+            func: func.clone(),
+        };
+
+        thread::spawn(move || task.execute())
     }
 }
