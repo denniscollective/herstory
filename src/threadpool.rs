943214a05cfb41dc88d1c55978110a1750f197c0
Dennis Collective <dennis.collective@gmail.com>
2017-09-06 13:06:44 -0600

    threadpool saves tasks

--- src/threadpool.rs
+++ src/threadpool.rs
@@ -1,51 +1,67 @@
 use std::thread;
 use std::sync::{Arc, Mutex};
 
-pub struct Threadpool {}
+pub struct Threadpool<I, F>
+where
+    F: Fn(&mut I) + Send + Sync + 'static,
+    I: Send + 'static,
+{
+    tasks: Option<Vec<Arc<Task<I, F>>>>,
+    handles: Option<Vec<thread::JoinHandle<()>>>,
+}
 
-impl Threadpool {
-    pub fn new(_worker_size: u32) -> Threadpool {
-        Threadpool {}
+impl<I, F> Threadpool<I, F>
+where
+    F: Fn(&mut I) + Send + Sync + 'static,
+    I: Send + 'static,
+{
+    pub fn new(_worker_size: u32) -> Threadpool<I, F> {
+        Threadpool {
+            tasks: None,
+            handles: None,
+        }
     }
 
-    pub fn batch<F, G>(self, collection: &Vec<Arc<Mutex<G>>>, func: F)
-    where
-        F: Fn(&mut G) + Send + Sync + 'static,
-        G: Send + 'static,
-    {
+    pub fn batch(mut self, collection: &Vec<Arc<Mutex<I>>>, func: F) {
         let func = Arc::new(func);
-        let tasks: Vec<thread::JoinHandle<()>> = collection
+        let tasks: Vec<Arc<Task<I, F>>> = collection
             .iter()
-            .map(|i| Task::clone_and_execute(&i, &func))
+            .map(|i| Arc::new(Task::new(i.clone(), func.clone())))
             .collect();
 
-        for handle in tasks {
+        self.handles = Some(tasks.iter().map(|task| self.run(task.clone())).collect());
+        self.tasks = Some(tasks);
+        self.wait();
+    }
+
+    fn wait(self) {
+        for handle in self.handles.unwrap() {
             handle.join().unwrap()
         }
     }
+    fn run(&self, task: Arc<Task<I, F>>) -> thread::JoinHandle<()> {
+        thread::spawn(move || task.execute())
+    }
 }
 
-struct Task<G, H> {
-    item: Arc<Mutex<G>>,
-    func: Arc<H>,
+struct Task<T, F> {
+    item: Arc<Mutex<T>>,
+    func: Arc<F>,
 }
 
-impl<G, H> Task<G, H>
+impl<T, F> Task<T, F>
 where
-    H: Fn(&mut G) + Send + Sync + 'static,
-    G: Send + 'static,
+    F: Fn(&mut T) + Send + Sync + 'static,
 {
     fn execute(&self) {
         let mut i = self.item.lock().unwrap();
         (self.func)(&mut i)
     }
 
-    fn clone_and_execute(i: &Arc<Mutex<G>>, func: &Arc<H>) -> thread::JoinHandle<()> {
-        let task = Task {
-            item: i.clone(),
-            func: func.clone(),
-        };
-
-        thread::spawn(move || task.execute())
+    fn new(i: Arc<Mutex<T>>, func: Arc<F>) -> Task<T, F> {
+        Task {
+            item: i,
+            func: func,
+        }
     }
 }
