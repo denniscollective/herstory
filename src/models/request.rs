74ad7a07a97883da5475168d8900c160c63b94f9
Dennis Collective <dennis.collective@gmail.com>
2017-09-19 22:55:57 -0400

    reorganize methods in request file

--- src/models/request.rs
+++ src/models/request.rs
@@ -10,47 +10,12 @@ use Status;
 use std::result::Result as RawResult;
 use errors::*;
 
-pub struct FileDownload(fs::File);
-impl Handler for FileDownload {
-    fn write(&mut self, data: &[u8]) -> RawResult<usize, WriteError> {
-        Ok(self.0.write(data).unwrap())
-    }
-}
-
-pub trait Requestable {
-    fn response_code(&mut self) -> Option<u32>;
-    fn perform(&mut self) -> Result<()>;
-}
-
-pub struct CurlRequest(Easy2<FileDownload>);
-
-impl Requestable for CurlRequest {
-    fn perform(&mut self) -> Result<()> {
-        self.0.perform().map_err(|err| err.into())
-    }
-
-    fn response_code(&mut self) -> Option<u32> {
-        self.0.response_code().ok()
-    }
-}
-
 pub struct Request {
     filename: String,
     status: Status,
     pub error: Option<Error>,
     pub raw: CurlRequest,
     pub response_code: Option<u32>,
-}
-
-impl fmt::Debug for Request {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Request {{ response_code: {}, error: {:?} }}",
-            self.response_code(),
-            &self.error
-        )
-    }
 }
 
 impl Request {
@@ -74,16 +39,16 @@ impl Request {
     pub fn perform_and_save(&mut self) -> Result<()> {
         match self.raw.perform() {
             Ok(_) => {
-                self.response_code = self.raw.response_code();
                 self.status = Status::Success;
+                self.response_code = self.raw.response_code();                
                 Ok(())
             }
 
             Err(err) => {
-                fs::remove_file(&self.filename).ok();
-                self.response_code = self.raw.response_code();
                 self.status = Status::Failure;
                 self.error = Some(err);
+                self.response_code = self.raw.response_code();
+                fs::remove_file(&self.filename).ok();                        
                 bail!("Request Failed")
             }
         }
@@ -100,5 +65,40 @@ impl Request {
 impl HasStatus for Request {
     fn status(&self) -> Status {
         self.status
+    }
+}
+
+impl fmt::Debug for Request {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Request {{ response_code: {}, error: {:?} }}",
+            self.response_code(),
+            &self.error
+        )
+    }
+}
+
+pub struct FileDownload(fs::File);
+impl Handler for FileDownload {
+    fn write(&mut self, data: &[u8]) -> RawResult<usize, WriteError> {
+        Ok(self.0.write(data).unwrap())
+    }
+}
+
+pub trait Requestable {
+    fn response_code(&mut self) -> Option<u32>;
+    fn perform(&mut self) -> Result<()>;
+}
+
+pub struct CurlRequest(Easy2<FileDownload>);
+
+impl Requestable for CurlRequest {
+    fn perform(&mut self) -> Result<()> {
+        self.0.perform().map_err(|err| err.into())
+    }
+
+    fn response_code(&mut self) -> Option<u32> {
+        self.0.response_code().ok()
     }
 }
